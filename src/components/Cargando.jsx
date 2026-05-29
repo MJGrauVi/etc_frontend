9a06a20ffa4a07f61ff0417e6405651ad3639e78
MJGrauVi <mariajosegrauvidal@gmail.com>
2026-05-29 11:18:32 +0200

    cambio cargando.jsx

--- src/components/Cargando.jsx
+++ src/components/Cargando.jsx
@@ -1,12 +1,15 @@
-import "./Cargando.css";
-// Muestro un indicador mientras se cargan los datos.
+import React from 'react'
+
 const Cargando = () => {
-  return (
-    <div className="contenedor-cargando">
-      <div className="spinner"></div>
-      <p>Cargando ...</p>
+    return (
+           <div className="flex flex-col items-center justify-center gap-3 p-4 sm:p-6 md:p-8 md:gap-4">
+      <div className="w-10 h-10 border-4 border-gray-200 rounded-full md:w-12 md:h-12 border-t-blue-500 animate-spin"></div>
+
+      <p className="text-sm md:text-base">
+        Cargando ...
+      </p>
     </div>
-  );
-};
+    )
+}
 
 export default Cargando;
